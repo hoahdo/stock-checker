e133d4c17caa092b53f2e214f2c96d2b65cee2a8
Hoa Do <hoa.do.ma@gmail.com>
2023-02-24 20:16:49 -0500

    set env variables

--- src/components/FetchPrices.jsx
+++ src/components/FetchPrices.jsx
@@ -1,20 +1,15 @@
-// import Finnhub from "./Finnhub";
 import { useState } from "react";
 import { finnhubClient } from "react-finnhub";
 import { dateToUnixTime, hoursToSecs } from "./utils";
 
-
-// const client = finnhubClient(`${process.env.API_KEY}`);
-
+const apiKey = import.meta.env.VITE_API_KEY
+const client = finnhubClient(apiKey);
 
 function FetchPrices({ fileType, fileData, checkDate }) {
 	const [stockData, setStockData] = useState();
 
     let timeFrom = dateToUnixTime(checkDate) + hoursToSecs(8)
     let timeTo = dateToUnixTime(checkDate) + hoursToSecs(20)
-
-    // console.log(`From: ${timeFrom}`)
-    // console.log(`To: ${timeTo}`)
 
 	function getStockPrice() {
 		client
