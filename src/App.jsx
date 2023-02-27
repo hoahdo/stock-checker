4fa7a0d58e5612e7928b65db73d626808c380ebf
Hoa Do <hoa.do.ma@gmail.com>
2023-02-26 20:10:05 -0500

    add footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Header from "./components/Header";
 import Upload from "./components/Upload";
+import Footer from "./components/Footer";
 
 function App() {
 	return (
 		<div className="App">
 			<Header />
 			<Upload />
+			<Footer />
 		</div>
 	);
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+	return (
+		<footer className="footer--container">
+			<p className="footer-text">&copy; Hoa</p>
+		</footer>
+	);
+}
+
+export default Footer;
